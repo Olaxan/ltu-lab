37d4b267ba784f1553d455c7abeb6c2deaf2e621
Fredrik Lind <fredrik.lind.96@gmail.com>
2018-09-17 22:49:18 +0200

    Changes in requestInt functionality

--- Lab2/Menu.py
+++ Lab2/Menu.py
@@ -13,34 +13,27 @@ menuItems = [
 	]
 
 def showMenu(items, title="Menu:", showExit = True):
-	while True:
-		print(title)
-		for num, name in enumerate(items, start=1):
-			print(num, ". ", name, sep='')
-		if showExit:
-			print("0. Exit\n")
-		valid, user = tryParse(input("> "))
-		if valid and (1 - int(showExit)) <= user <= len(menuItems):
-			return user
-		else:
-			title = "\nPlease select an item between "+str(1 - int(showExit))+"-"+str(len(menuItems))+"."
-
-def requestInt(title, prompt = "> ", error = "\nPlease enter an integer.", exitChar = None):
 	print(title)
+	for num, name in enumerate(items, start=1):
+		print(num, ". ", name, sep='')
+	if showExit:
+		print("0. Exit\n")
+		return requestInt(lower=0, upper=len(menuItems), error="\nPlease select an item between "+str(1 - int(showExit))+"-"+str(len(menuItems))+".")
+
+def requestInt(prompt = "> ", error = "\nPlease enter an integer.", lower = float('-inf'), upper = float('inf'), exitChar = None):
 	if (exitChar != None):
 		print("Enter", exitChar, "to exit.")
 	while True:
 		user = input(prompt)
 		valid, out = tryParse(user)
-		if valid:
+		if valid and lower <= out <= upper:
 			if (user == exitChar):
-				break
+				return None
 			return out
 		else:
 			print(error)
 
 while True:
-	print()
 	result = showMenu(menuItems)
 
 	if (result == 0):
@@ -51,4 +44,6 @@ while True:
 		print(rek.tvarsumman(requestInt("Letter sum: ")))
 	elif (result == 3):
 		print("x =", rek.solve(lambda x : x**2-1, requestInt("Solver start: "), 0.0001))
+
+	print()
 
