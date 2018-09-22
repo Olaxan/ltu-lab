ff5b1295584c9b62fc72bd91999f114b92916ce3
Fredrik Lind <fredrik.lind.96@gmail.com>
2018-09-23 01:09:52 +0200

    Minor changes

--- Labb4/Labb4.py
+++ Labb4/Labb4.py
@@ -1,5 +1,8 @@
 import utils
 import os
+import command
+
+test = command.Add()
 
 print("TelePOST Catalogue System v0.01 ALPHA")
 
@@ -26,27 +29,6 @@ while True:
 			curr.append(sub)
 		
 		print(valid)
-
-
-
-
-	#if com[0] in com_data:					#if the initial command is valid...
-	#	for sub_com in com_data[com[0]]:	#loop through subcommands...
-	#		if sub_com in com:				#if a subcommand is recognized...
-	#			ind = com.index(sub_com)	#find index of subcommand in tuple of valid subcommands (see com_data)
-	#			#print("command '{}' accepted (index {})".format(sub_com, ind))
-	#			if len(com) - 1 > ind and com[ind + 1] not in com_data[com[0]]:		#if a valid subcommand is found, check if it's followed by a value (not a subcommand)
-	#				print(com[ind].upper())
-	#				for x in range(ind + 1, len(com)):		#loop through next values, until the end of the command string...
-	#					if com[x] in com_data[com[0]]:		#or until a valid subcommand is found...
-	#						del com[ind:x]					#in which case, delete these subcommands (leaving only the main command string)...
-	#						break							#then break TODO: make natural end-of-loop also delete commands
-	#					print (com[x])
-	#				#print("{0}: {1}".format(sub_com, com[ind + 1]))
-	#				#del com[ind:len(com)]
-	#			else:
-	#				print("SYNTAX: '{0}' {1}".format(com[0], com_data[com[0]]))
-	#	print(com)
 
 #add Tord Fredrik Gustav Lind (num 0733573176 alias Toady)
 #0	 1    2       3      4     5   6          7     8
---
+++ Labb4/command.py
@@ -0,0 +1,7 @@
+class Command:
+	def __init__(self):
+		pass
+
+class Add(Command):
+	def __init__(self):
+		pass
